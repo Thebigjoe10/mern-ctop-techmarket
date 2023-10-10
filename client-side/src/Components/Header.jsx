73ca962398c1a991f51c06fbfc44f62de074fb26
thebigjoe10 <jojosr001@gmail.com>
2023-10-10 15:19:12 -0700

    change header background

--- client-side/src/Components/Header.jsx
+++ client-side/src/Components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-green-100 shadow-md">
+    <header className="bg-gradient-to-l from-green-200 to-green-500 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
@@ -17,7 +17,7 @@ export default function Header() {
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
-          <FaSearch className="text-green-600" />
+          <FaSearch className="text-green-500" />
         </form>
         <ul className="flex gap-4">
           <Link to="/">
