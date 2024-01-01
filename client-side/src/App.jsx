07e4f539b0dd7f5d84308fbe299bddaf2479a0ac
thebigjoe10 <jojosr001@gmail.com>
2024-01-01 11:37:26 -0800

    Fixed spelling error for pages

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import SignIn from './Pages/SignIn';
 import SignUp from './Pages/SignUp';
-import About from './pages/About';
+import About from './Pages/About';
 import Profile from './Pages/Profile';
 import Header from './Components/Header';
 import PrivateRoute from './Components/PrivateRoute';
