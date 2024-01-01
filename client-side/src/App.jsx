4b5e5de8260fe2acf8c631a6285dd53f57a9cc18
thebigjoe10 <jojosr001@gmail.com>
2024-01-01 11:36:36 -0800

    fix spelling error in for Components and Pages

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import Home from './Pages/Home';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
 import About from './pages/About';
 import Profile from './Pages/Profile';
-import Header from './components/Header';
-import PrivateRoute from './components/PrivateRoute';
+import Header from './Components/Header';
+import PrivateRoute from './Components/PrivateRoute';
 import CreateListing from './Pages/CreateListing';
 import UpdateListing from './Pages/UpdateListing';
 import Listing from './Pages/Listing';
